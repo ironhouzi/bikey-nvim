f29b810558adf8518fe3ad0b67ccb15665f67d79
Robin Skahjem-Eriksen <robin@skahjem-eriksen.no>
2022-01-28 12:10:32 +0100

    Fix: parse gnome dbus messages
    Added cjson requirement.

--- lua/bikey/init.lua
+++ lua/bikey/init.lua
@@ -1,33 +1,14 @@
 local dbus_proxy = require'dbus_proxy'
+local cjson = require 'cjson'
 
 
 local function set_config()
   local bikey_dbus = nil
-  local os = nil
+  local de = nil
   local gnome_input_sources = 'imports.ui.status.keyboard.'
     .. 'getInputSourceManager().inputSources'
 
   local os_configs = {
-    ubuntu = {
-      connection = {
-        bus = dbus_proxy.Bus.SESSION,
-        name = 'org.gnome.Shell',
-        interface = 'org.gnome.Shell',
-        path = '/org/gnome/Shell',
-      },
-      normal_mode = function()
-        bikey_dbus:Eval(gnome_input_sources .. '[0].activate()')
-      end,
-      insert_mode = function()
-        local i = vim.b.bikey_active and 1 or 0
-        bikey_dbus:Eval(gnome_input_sources .. '[' .. i .. '].activate()')
-      end,
-      kbd_layout_list = function()
-        local layouts = bikey_dbus:Eval(gnome_input_sources)
-        print(vim.inspect(layouts))
-        return layouts
-      end,
-    },
     kde = {
       connection = {
         bus = dbus_proxy.Bus.SESSION,
@@ -42,27 +23,70 @@ local function set_config()
         bikey_dbus:setLayout(vim.b.bikey_active and 1 or 0)
       end,
       kbd_layout_list = function()
+        local result = {}
         local layouts = bikey_dbus:getLayoutsList()
-        print(vim.inspect(layouts))
-        return layouts
+        -- layouts fmt: {{"us", "", "English (US"}, ...}
+        for _, layout in ipairs(layouts) do
+          table.insert(result, {
+            id = layout[1],
+            fullname = layout[3],
+            shortname = vim.inspect(layout[3]:sub(1,2):lower()),
+          })
+        end
+        return result
+      end,
+    },
+    gnome = {
+      connection = {
+        bus = dbus_proxy.Bus.SESSION,
+        name = 'org.gnome.Shell',
+        interface = 'org.gnome.Shell',
+        path = '/org/gnome/Shell',
+      },
+      normal_mode = function()
+        bikey_dbus:Eval(gnome_input_sources .. '[0].activate()')
+      end,
+      insert_mode = function()
+        local i = vim.b.bikey_active and 1 or 0
+        bikey_dbus:Eval(gnome_input_sources .. '[' .. i .. '].activate()')
+      end,
+      kbd_layout_list = function()
+        local result = {}
+        local layouts = bikey_dbus:Eval(gnome_input_sources)
+        -- layouts fmt: { true, '{"0":{"__caller__":null,"type":"xkb","id":"us",
+        --                "displayName":"English (US)","_shortName":"en",
+        --                "index":0,"properties":null,"xkbId":"us",
+        --                "_signalConnections":[...],"_nextConnectionId":3}, ... }'}
+        -- TODO: Find way to avoid explicit deserialization with cjson
+        for _, layout in pairs(cjson.decode(layouts[2])) do
+          table.insert(result, {
+            id = layout['id'],
+            fullname = layout['displayName'],
+            shortname = layout['_shortName'],
+          })
+        end
+        return result
       end,
     },
   }
 
-  for distro, cfg in pairs(os_configs) do
+  for desktop_env, cfg in pairs(os_configs) do
     if pcall(function() dbus_proxy.Proxy:new(cfg.connection) end) then
       bikey_dbus = dbus_proxy.Proxy:new(cfg.connection)
-      os = distro
+      de = desktop_env
       break
     end
   end
 
-  assert(os ~= nil)
+  assert(de ~= nil)
   assert(bikey_dbus ~= nil)
-  return os_configs[os]
+  return os_configs[de]
 end
 
 local bikey = set_config()
+
+local function get_status()
+end
 
 local function toggle_bikey()
   vim.b.bikey_active = not vim.b.bikey_active
